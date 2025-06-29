ad51c98d06c7411a3376a0a7bfff6f516d1864d6
Gaurangi25 <gaurangi250704@gmail.com>
2025-06-30 02:56:01 +0530

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  setInterval(increase, 1000);
-
-  const now = new Date().toLocaleTimeString();
-
-  const [time, setTime] = useState(now);
+  const currentTime = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(currentTime);
+  console.log(currentTime);
 
   function increase() {
     const newTime = new Date().toLocaleTimeString();
     setTime(newTime);
+  
   }
+
+  useEffect(() => {
+    const interval = setInterval(increase, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="container">
       <h1>{time}</h1>
-      <button onClick={increase}>Get Time</button>
+      <button onClick={increase}>Time</button>
     </div>
   );
 }
