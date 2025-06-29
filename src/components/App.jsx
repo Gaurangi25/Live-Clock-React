8057e1d1c3b23a47153078ad448483dc5f3bf34d
Gaurangi25 <gaurangi250704@gmail.com>
2025-06-30 02:50:31 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function App() {
+  setInterval(increase, 1000);
+
+  const now = new Date().toLocaleTimeString();
+
+  const [time, setTime] = useState(now);
+
+  function increase() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={increase}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
